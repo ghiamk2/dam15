bbc6e9f0ca7ef2395a0c6423140b90ecdb885c1a
ghia16v <Xabier@pcsantur>
2017-04-29 19:35:59 +0200

    Mi primer commit

---
+++ PrimerProyecto/src/PrimeraClase.java
@@ -0,0 +1,17 @@
+
+public class PrimeraClase {
+PrimeraClase(){
+	String nombre="xabier";
+	String apellido="luque";
+	int telefono= 94448844;
+};
+
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+PrimeraClase obj1 = new PrimeraClase();
+
+	}
+
+}
